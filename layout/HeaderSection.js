3e6a30103bf28069dc94af482923b4e3b94b7423
Mathias Rechtzigel <MathiasaurusRex@gmail.com>
2018-09-10 17:00:36 -0500

    Updating some copy

--- layout/HeaderSection.js
+++ layout/HeaderSection.js
@@ -6,9 +6,13 @@ const HeaderSection = props => (
       <div className="tc-hf--header-copy">
         <div className="flex-seperate">
           <div className="" />
-          <div className="stamp-outline">
-            <span>Sign Up</span>
-          </div>
+          <a
+            href="https://github.com/MathiasaurusRex/Twin-Cities-Hacktoberfest"
+            className="stamp-outline"
+            target="_blank"
+          >
+            <span>Github Repository</span>
+          </a>
         </div>
         <h1 className="tc-hf--title">
           <span>Twin Cities 2018</span>
@@ -25,9 +29,10 @@ const HeaderSection = props => (
               software.
             </p>
             <p>
-              Everyone can learn to create amazing things on the internet,
-              unfortunately that first step into a new world can be the hardest
-              and if it's a bad experience some may be turned off of it.
+              Everyone can learn to create amazing things on the internet, but
+              that first step into a new world can be the hardest and if it's a
+              bad experience some may be turned off of it -- that's why we want
+              to guide those who could use a helping hand.
             </p>
           </div>
         </section>
@@ -37,9 +42,8 @@ const HeaderSection = props => (
           </h2>
           <div className="content-box">
             <p>
-              Those who participate will create one section of this webpage. You
-              get an area from the top of the screen to the bottom of the screen
-              to tell your story.
+              Those who participate will create a section of this webpage -- a
+              full screen -- to tell your story.
             </p>
           </div>
         </section>
@@ -48,16 +52,33 @@ const HeaderSection = props => (
             <span className="tc-hf--subtitle-top">How we'll do it:</span>
           </h2>
           <div className="content-box">
-            <p>We'll be using the following tools:</p>
-            <ul>
-              <li>Github / Git</li>
-              <li>HTML / CSS / Javascript</li>
-              <li>React / Next.JS</li>
-            </ul>
+            <p>
+              We will be available throughout the month of October to help
+              anyone create a section of this website -- for free and regardless
+              of skill level. You'll be getting a taste of every aspect of the
+              creation process from concept creation to final implementation --
+              and in the end you'll be able to see your section along with
+              everyone elses on this website!
+            </p>
+            <p>Details and how to's will be available shortly!</p>
+          </div>
+        </section>
+        <section className="copy-section">
+          <h2 className="tc-hf--subtitle">
+            <span className="tc-hf--subtitle-top">Interested?</span>
+          </h2>
+          <div className="content-box">
+            <p>
+              Details will be coming before the end of September -- but you can
+              shoot me an email at{" "}
+              <a href="mailto:Mathias@Trailguide.io">Mathias@Trailguide.io</a>{" "}
+              if you would like to schedule a 1:1, small or large group session
+              or even just to help out!
+            </p>
           </div>
         </section>
       </div>
-      <button className="tc-hf--header-button">Sign Up</button>
+      {/* <button className="tc-hf--header-button">Sign Up</button> */}
     </div>
 
     <style jsx>{`
@@ -83,6 +104,12 @@ const HeaderSection = props => (
         font-family: "Arvo", serif;
         font-size: 1rem;
         line-height: 1;
+        transition: 0.3s all ease;
+      }
+
+      .stamp-outline:hover {
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19),
+          0 6px 6px rgba(0, 0, 0, 0.23);
       }
 
       .stamp-outline > * + * {
