a6f111648ae9bf81e4f1963c67a9b9e704e823bd
Randala <randalab6@gmail.com>
2018-10-02 14:21:38 -0500

    first_component

---
+++ pages/playground.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+
+import Layout from "../layout/Layout";
+import Randala from "../personalsections/randala/Randala.js";
+
+const playground = props => (
+  <Layout
+    title="Twin Cities Hacktoberfest 2018"
+    metaDescription="Twin Cities Hacktoberfest is organized by M.R., but created with love and care from individuals just like you across the Minnesota."
+    siteURL="https://www.mathiasrechtzigel.com"
+  >
+
+
+    <Randala />
+
+  </Layout>
+);
+
+export default playground;
